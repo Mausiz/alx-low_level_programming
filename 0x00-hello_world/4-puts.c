ab3c34e53715b21dde0913919cf5d4c51100012b
Mausiz <mosesmefray@gmail.com>
2023-06-15 17:37:39 -0700

    first progrem using puts instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  prints exactly 'Programming is like building a multilingual puzzle
+ *  Return 0
+ *  function used is 'puts'
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
