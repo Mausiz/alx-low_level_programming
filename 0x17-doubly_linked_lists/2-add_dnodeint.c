9b0ae0c83667e7b4104a9caf522166c34fd778af
Mausiz <mosesmefray@gmail.com>
2023-11-09 17:23:24 -0800

    hjk

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,7 +6,7 @@
  * @n: value of element
  * Return: the address of the new element
  */
-dlistint_t *add_dnoteint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	
@@ -18,8 +18,10 @@ dlistint_t *add_dnoteint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
+
 	if (*head != NULL)
 		(*head)->prev = new;
+
 	*head = new;
 	return (new);
 }
